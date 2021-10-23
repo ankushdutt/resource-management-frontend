f125c88fbb53f5fca95a0b9ec47a216e712e066a
Ankush Dutt <ankushd100@gmail.com>
2021-10-23 15:20:54 +0530

    Lecture Hall: Implement handle onClick for LectureHall button

---
+++ src/modules/LectureHall/App.js
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      Edit this in src/modules/LectureHall/App.js
+      <a className="text-blue-500 mt-4" href="/">
+        Back
+      </a>
+    </div>
+  );
+}
--- src/modules/LectureHall/index.js
+++ src/modules/LectureHall/index.js
@@ -1,7 +1,15 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+
 export default function index() {
   return (
     <div>
-      <button className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600">
+      <button
+        className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600"
+        onClick={() => {
+          ReactDOM.render(<App />, document.getElementById("root"));
+        }}
+      >
         <h3 className="text-2xl font-bold">Lecture Hall Management &rarr;</h3>
         <p className="mt-4 text-xl">
           Edit this text in /src/modules/LectureHall/index.js
