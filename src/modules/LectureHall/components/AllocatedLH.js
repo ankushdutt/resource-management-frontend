ddfc8ec125d97ba83d02b05c3240ce51b8b76076
Ankush Dutt <ankushd100@gmail.com>
2021-10-31 13:43:27 +0530

    [Lecture Hall]: Implement delete handler in allocatedLH

--- src/modules/LectureHall/components/AllocatedLH.js
+++ src/modules/LectureHall/components/AllocatedLH.js
@@ -1,12 +1,27 @@
 import Lecturehall from "./Lecturehall";
+import { useState } from "react";
 
 export default function AllocatedLH() {
+  const [allocatedLH, setAllocatedLH] = useState([1, 5, 7]);
+
+  function deleteHandler(id) {
+    setAllocatedLH(allocatedLH.filter((lh) => lh !== id));
+  }
+
   return (
-    <div>
-      <h1>Lecture Halls Allocated to you:-</h1>
-      <Lecturehall text='LT1' />
-      <Lecturehall text='LT5' />
-      <Lecturehall text='LT7' />
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="py-5">Lecture Halls Allocated to you:</h1>
+      {allocatedLH.map((lh) => (
+        <div>
+          <Lecturehall key={lh} id={lh} />
+          <button
+            className="p-2 border-2 rounded text-red border-red hover:text-red-400 mb-2"
+            onClick={() => deleteHandler(lh)}
+          >
+            Remove
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
--- src/modules/LectureHall/components/Lecturehall.js
+++ src/modules/LectureHall/components/Lecturehall.js
@@ -1,19 +1,7 @@
 export default function Lecturehall(props) {
-  function deleteHandler() {
-
-  }
-
   return (
-    <div class="h-100 flex justify-center bg-teal-lightest font-sans">
-      {/* <h2>{props.text}</h2> */}
-      {/* <div >
-            <button  onClick={deleteHandler}>Delete</button>
-          </div> */}
-      <div class="flex mb-4 items-center">
-        <p class="w-full text-grey-darkest">{props.text}</p>
-        <button class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red" onClick={deleteHandler}>Remove</button>
-      </div>
+    <div className="h-100 flex justify-center bg-teal-lightest font-sans items-center">
+      <p className="w-full text-grey-darkest">{props.id}</p>
     </div>
   );
 }
-
