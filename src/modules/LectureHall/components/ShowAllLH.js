9a29ecf8df6707dc3f598178e2b681ada0a2ec4f
Ankush Dutt <ankushd100@gmail.com>
2021-11-02 23:26:58 +0530

    [Lecture Hall]: Show more data in ShowAllLH

--- src/modules/LectureHall/components/ShowAllLH.js
+++ src/modules/LectureHall/components/ShowAllLH.js
@@ -1,26 +1,40 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 
 export default function ShowAllLH() {
-  const [LH, setLH] = useState([{ lh_id: "Fetching data from server..." }]);
-  const didMount = useRef(false);
-  useEffect(() => {
-    if (!didMount.current) {
-      didMount.current = true;
-      fetch(`https://lecture-hall-backend.herokuapp.com/lecturehall/all`, {
-        method: "GET",
-      })
-        .then((response) => response.json())
-        .then((data) => setLH(data));
-    }
-  });
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchServerData = async () => {
+    fetch(`https://lecture-hall-backend.herokuapp.com/lecturehall/all`, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => setData(data));
+    setIsLoading(false);
+  };
+
+  useEffect(() => fetchServerData(), []);
+
   return (
     <div>
       <div>Show All</div>
-      {LH.map((LH) => (
+
+      {isLoading || !data ? (
+        <div>Fetching lecture halls data from server...</div>
+      ) : (
         <div>
-          <div>{LH.lh_id}</div>
+          {data.map((obj) => (
+            <div key={obj.lh_id}>
+              <div>ID: {obj.lh_id}</div>
+              <div>Max Capacity: {obj.max_capacity}</div>
+              <div>
+                Director Permission required:
+                {obj.special_permission_req ? " Yes" : " No"}
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      )}
     </div>
   );
 }
