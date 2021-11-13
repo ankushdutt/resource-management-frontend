ee5f39b66bb1acf4ced17e77a0f27f044c918d5f
aayushbudania <aayusbudania002@gmail.com>
2021-11-13 18:25:04 +0530

    login

--- src/modules/LectureHall/components/Login.js
+++ src/modules/LectureHall/components/Login.js
@@ -30,7 +30,7 @@ export default function Login({ setIsLoggedIn }) {
           <p className="text-red-500 text-xs italic">Please choose a password.</p>
         </div>
         <div className="flex items-center justify-between">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
             Sign In
           </button>
         </div>
