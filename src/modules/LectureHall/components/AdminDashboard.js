5b2751ccb0a04fed1fe38cd91b75f5f7efc8d66e
Ankush Dutt <ankushd100@gmail.com>
2021-12-21 23:26:12 +0530

    [Lecture Hall]: Add upload timetable button

--- src/modules/LectureHall/components/AdminDashboard.js
+++ src/modules/LectureHall/components/AdminDashboard.js
@@ -1,6 +1,13 @@
 export default function AdminDashboard() {
   return (
     <div className="w-1/3 m-auto p-2">
+      <div className="m-auto">
+        Upload Time Table CSV
+        <input type="file" />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Allocate from Time Table
+        </button>
+      </div>
       <button
         className="w-full bg-blue-500 rounded p-3 m-3"
         onClick={() => {
