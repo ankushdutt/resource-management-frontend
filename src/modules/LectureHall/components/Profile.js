271ee1ee91aca49a5d049865d21ed26a653a7cc4
Hemang Goyal <hemanggoyal8@gmail.com>
2022-01-09 19:44:03 +0530

    ok

--- src/modules/LectureHall/components/Profile.js
+++ src/modules/LectureHall/components/Profile.js
@@ -1,13 +1,32 @@
+import React from 'react';
+import { useHistory } from "react-router-dom";
+
+
 export default function Profile() {
+
+  const history = useHistory();
+  const handleRoute = () =>{ 
+    history.push("/UpdateEmail");
+  }
+  
   return (
     <div className="w-1/3 m-auto p-2">
       <button
         className="w-full bg-blue-500 rounded p-3 m-3"
         onClick={() => {
-          // Update profile
+          {handleRoute}
+          // Update email
         }}
       >
-        Update Email and Password
+        Update Email
+      </button>
+      <button
+        className="w-full bg-blue-500 rounded p-3 m-3"
+        onClick={() => {
+          // Update password
+        }}
+      >
+        Update Password
       </button>
       <button
         className="w-full bg-blue-500 rounded p-3 m-3"
