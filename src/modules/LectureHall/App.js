7cecf2e1f86b15fca44f99c20ffba4106b2718fb
Hemang Goyal <hemanggoyal8@gmail.com>
2022-01-10 16:28:50 +0530

    ok

--- src/modules/LectureHall/App.js
+++ src/modules/LectureHall/App.js
@@ -7,11 +7,8 @@ import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import AdminDashboard from "./components/AdminDashboard";
-<<<<<<< HEAD
 import UpdateEmail from "./components/UpdateEmail";
-=======
 import DeleteUser from "./components/DeleteUser";
->>>>>>> 49c82983d10aed1b610b151ad4dbdf2c8ab0d1ee
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -62,17 +59,13 @@ export default function App() {
         <Route path="/lecturehall/profile">
           {isLoggedIn ? <Profile /> : <Redirect to="/lecturehall/login" />}
         </Route>
-<<<<<<< HEAD
         <Route path="/UpdateEmail">
             {isLoggedIn ? <UpdateEmail /> : <Redirect to="/lecturehall/login" />}
         </Route>
-        <Route path="/lecturehall/admin">
-=======
         <Route path="/lecturehall/admin/delete-user">
           {isAdmin ? <DeleteUser/> : <Redirect to="/lecturehall/login" />}
         </Route>
         <Route exact path="/lecturehall/admin">
->>>>>>> 49c82983d10aed1b610b151ad4dbdf2c8ab0d1ee
           {isAdmin ? <AdminDashboard /> : <Redirect to="/lecturehall/login" />}
         </Route>
       </Switch>
