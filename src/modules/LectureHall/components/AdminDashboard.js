a11ee817b1c8e130719a9ded0cd4da0df177ce6b
Ankush Dutt <ankushd100@gmail.com>
2022-01-11 09:30:57 +0530

    small changes

--- src/modules/LectureHall/components/AdminDashboard.js
+++ src/modules/LectureHall/components/AdminDashboard.js
@@ -130,7 +130,7 @@ export default function AdminDashboard() {
           history.push("/lecturehall/admin/delete-user");
         }}
       >
-        Delete User Accounts
+        Show and Delete User Accounts
       </button>
       <button
         className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-3"
@@ -139,7 +139,7 @@ export default function AdminDashboard() {
           history.push("/lecturehall/admin/update-lh");
         }}
       >
-        Delete Lecture Halls
+        Show and Delete Lecture Halls
       </button>
       <button
         className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-3"
